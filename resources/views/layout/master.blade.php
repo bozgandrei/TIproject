92570a270350b26bd53069731887d3151ad9678f
bozgandrei <bozgandrei@yahoo.com>
2016-03-02 14:42:50 +0200

    Interfata Login

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{URL::asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('css/style.css')}}">
+    <script type="text/javascript" src="{{URL::asset('/js/jquery-1.12.0.min.js')}}"></script>
+    <script type="text/javascript" src="{{URL::asset('js/bootstrap.min.js')}}"></script>
+    <script type="text/javascript" src="{{URL::asset('/js/app.js')}}"></script>
+    <link rel="stylesheet" href="{{URL::asset('css/datepicker.css')}}">
+    <script type="text/javascript" src="{{URL::asset('/js/bootstrap-datepicker.js')}}"></script>
+
+</head>
+
+<body>
+<div class="container">
+    @yield('nav')
+
+
+    @yield('content')
+
+
+</div>
+</body>
+
+
+</html>
